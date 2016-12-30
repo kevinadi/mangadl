4e20da60891396940af477bae95936d2e780521e
Kevin Adistambha <kevinadi@gmail.com>
2016-12-30 15:21:12 +1100

    Refactored the download process out of main

--- mangadl.py
+++ mangadl.py
@@ -18,17 +18,18 @@ sites = {
     }
 }
 
-class Gevent_queue:
-    def __init__(self, pages, workers=4):
-        self.tasks = Queue()
-        self.pages = pages  # format is [(pagenum, pageurl)]
+class Gevent_download:
+    def __init__(self, tasks, workers=4, filename='out'):
+        self.queue = Queue()
+        self.tasks = tasks  # format is [(pagenum, pageurl)]
         self.workers = workers  # num of workers
         self.out = []  # list of results
+        self.filename = filename.replace('/', '')
 
-    def worker(self,n):
-        while not self.tasks.empty():
+    def worker(self,n, func):
+        while not self.queue.empty():
             start_time = time()
-            task = self.tasks.get()
+            task = self.queue.get()
 
             # get page
             res_raw = requests.get(task[1])
@@ -40,36 +41,53 @@ class Gevent_queue:
             jpg = Image.open(StringIO(jpg_raw.content))
 
             # finish task
-            self.out.append((task[0], img, jpg.size))
+            self.out.append((task[0], img, jpg))
             print 'Worker {0} finished {1} in {2}'.format(n, task, time()-start_time)
 
     def execute(self):
-        for page in self.pages:
-            self.tasks.put_nowait(page)
+        for task in self.tasks:
+            self.queue.put_nowait(task)
         gevent.joinall([gevent.spawn(self.worker, x) for x in xrange(self.workers)])
+        self.out.sort()
         return self.out
+
+    def save_jpg(self):
+        for img in self.out:
+            img[2].save_jpg(self.filename + '-' + str(img[0]) + '.jpg')
+
+class Download:
+    def __init__(self, name):
+        self.name = name
+        self.filename = name.replace('/', '')
+
+    def execute(self):
+        # get page 1 + page 1 img + links to other pages
+        print 'Getting page list'
+        page_raw = requests.get(sites['mangareader']['url'] + self.name)
+        page = BeautifulSoup(page_raw.text, 'html.parser')
+        img1 = sites['mangareader']['img'](page)
+
+        jpg_raw = requests.get(img1)
+        jpg = Image.open(StringIO(jpg_raw.content))
+        jpg.save_jpg(self.filename + '-0.jpg')
+
+        # TODO save into a cbz
+
+        # get list of pages
+        pages = sites['mangareader']['page_list'](page)
+        pages = [(i+1,p) for i, p in enumerate(pages[1:])]
+        # pprint(pages)
+
+        # get multiple pages
+        start_time = time()
+        q = Gevent_download(pages, workers=10, filename=self.filename)
+        res = q.execute()
+        # res.sort()
+        # pprint(res)
+        q.save()
+        print '>>> Time:', time()-start_time
 
 
 if __name__ == '__main__':
-
-    # get page 1 + page 1 img + links to other pages
-    print 'Getting page list'
-    page_raw = requests.get(sites['mangareader']['url'] + '/naruto/2')
-    page = BeautifulSoup(page_raw.text, 'html.parser')
-    img1 = sites['mangareader']['img'](page)
-    # TODO get page 1 jpg here
-
-    # get list of pages
-    pages = sites['mangareader']['page_list'](page)
-    pages = [(i+1,p) for i, p in enumerate(pages[1:])]
-    # pprint(pages)
-
-    # get multiple page
-    start_time = time()
-    q = Gevent_queue(pages[:12], workers=4)
-    res = q.execute()
-    res.sort()
-    pprint(res)
-    print '>>> Time:', time()-start_time
-
-
+    d = Download('/naruto/5')
+    d.execute()
